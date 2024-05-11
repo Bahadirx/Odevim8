a40dfa80c55eaf502789599faea6f0f41206fbcb
Bahadirx <146478657+Bahadirx@users.noreply.github.com>
2024-05-11 14:06:29 +0300

    Add files via upload

---
+++ odev8/Controllers/PersonelController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace odev8.Controllers
+{
+    [ApiController]
+public class PersonelController : Controllerbase
+{
+public string Get(){
+    return "1";
+}
+}
+
+
+
+
+
+}
